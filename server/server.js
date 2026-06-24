d8404b4d7e34823fb727fd8e1732c9cba13c0fbc
Aleksandr Dubovik <95865961+al2bovek@users.noreply.github.com>
2026-06-24 08:28:10 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -35,8 +35,9 @@ const PORT = process.env.PORT || 3000;
 
 const start = async () => {
   await runMigrations();
-  app.listen(PORT, '0.0.0.0',() => {
+  app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server listening on port http://localhost:${PORT}`);
+     console.log(`Client URL: ${process.env.CLIENT_URL}`);
   });
 };
 
